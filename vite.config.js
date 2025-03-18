76ca352240fa6957ac795387baeb1a6fa2655d79
Ryan Latimer <rlatimer96@harborps.org>
2025-03-18 18:52:40 -0400

    Push

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Avexel.co/', // This should match your repo name
+  base: '/', // Changed to root path for custom domain deployment
 });
