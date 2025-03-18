c8e81f4ba91a9e7825a8c7fd9e8d96a1f198f54e
Ryan Latimer <rlatimer96@harborps.org>
2025-03-18 19:49:32 -0400

    Thus should be good now.

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,22 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   build: {
     outDir: 'build',
     assetsDir: 'assets',
     sourcemap: true,
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith('.woff2')) {
+            return 'fonts/[name][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        }
+      }
+    }
   },
   server: {
     headers: {
