82f24ff5d1c2cc6a8e522db14a82c77a4dc06055
Ryan Latimer <rlatimer96@harborps.org>
2025-03-19 10:18:59 -0400

    more fixes.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Changed from './' to '/' for absolute paths
+  base: './',  // Changed from '/' to './' for relative paths
   build: {
     outDir: 'build',
     assetsDir: 'assets',
