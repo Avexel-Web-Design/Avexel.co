db97cc064b03bf5cf045624f053bef76b62a63b9
Ryan Latimer <rlatimer96@harborps.org>
2025-04-08 13:55:15 -0400

    Maybe fix that stupid font

---
+++ dist/assets/useScrollReveal-q8y6LGW0.js
@@ -0,0 +1,2 @@
+import{r as l}from"./index-tofzQzhC.js";const i=()=>{l.useEffect(()=>{const t=document.querySelectorAll(".reveal"),e=()=>{const r=window.innerHeight,n=150;t.forEach(o=>{o.getBoundingClientRect().top<r-n&&o.classList.add("active")})};return e(),window.addEventListener("scroll",e),()=>{window.removeEventListener("scroll",e)}},[])};export{i as u};
+//# sourceMappingURL=useScrollReveal-q8y6LGW0.js.map
